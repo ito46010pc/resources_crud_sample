1247112e25215873c3f3d9ec5f0f3761bccc6fb1
ito46010 <g.itoitou@gmail.com>
2020-07-04 23:38:02 +0900

    resources crud_sample

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  # 一覧画面に対応するアクション
+  def index
+    @users = User.all
+  end
+
+
+  # 新規作成画面に対応するアクション
+  def new
+    @user = User.new
+  end
+
+  # 新規登録するためのアクション
+  def create
+    User.create(user_params)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+
+
+  # 削除をするためのアクション
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+      
+
+end
