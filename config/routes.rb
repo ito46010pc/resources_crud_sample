0c8c49bfa0c7ebea745d71a6a04fd2b774a76c67
ito46010 <g.itoitou@gmail.com>
2020-07-10 23:32:12 +0900

    used resources crud_sample

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  # 一覧画面へのルーティング
-  get '/users', to: 'users#index'
-
-  # 新規投稿画面へのルーティング
-  get '/users/new', to: 'users#new'
-  # 新規登録を行うためのルーティング
-  post '/users', to: 'users#create'
-
-  get '/users/:id/edit', to: 'users#edit'
-
-  patch '/users/:id', to: 'users#update'
-  # 削除を行うためのルーティング
-  delete '/users/:id', to: 'users#destroy'
-
   resources :users
-
 end
